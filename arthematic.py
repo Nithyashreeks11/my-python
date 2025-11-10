4f8414832dc1611b77e7200b610a326aa75f82d9
Nithyashreeks11 <yasodanithya@gmail.com>
2025-11-10 16:35:50 +0530

    Create arthematic.py

---
+++ arthematic.py
@@ -0,0 +1,21 @@
+
+rint(from binascii import b2a_base64
+a=int(input("enternumber 1"))
+b=int(input("enter number2"))
+#additionp
+sum=a+b
+print("addition is",sum)
+#subration
+sum=a-b
+print("subration is",sum)
+#multiplication
+sum=a*b
+print("multiplication is",sum)
+#division
+sum=a/b
+print("division is",sum)
+#floor division
+sum=a//b"floor division is",sum)
+#moudlus
+sum=a%b
+print("moudlus is",sum)
