65e4e61ed3e9e67cfc9318b1405d9f10899b67bf
Nithyashreeks11 <yasodanithya@gmail.com>
2025-11-12 15:33:35 +0530

    Create user notifications.py

---
+++ user notifications.py
@@ -0,0 +1,7 @@
+def usernotification(x,y):
+  name=input("enter your name")
+  age=int(input("enter your age"))
+  roll=int(input("enter your rool"))
+  return name ,age,roll
+usernotification(2,4)
+  
