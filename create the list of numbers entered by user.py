fa89d00006ca0c3ca193edbaec99db24994f44b5
Nithyashreeks11 <yasodanithya@gmail.com>
2025-11-14 15:10:24 +0530

    Create create the list of numbers entered by user.py

---
+++ create the list of numbers entered by user.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number a"))
+b=int(input("enter the number b"))
+c=int(input("enter the number c"))
+d=int(input("enter the number d"))
+e=int(input("enter the number e"))
+n=[a,b,c,d,e]
+print(n)
