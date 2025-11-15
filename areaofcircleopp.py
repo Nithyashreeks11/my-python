fde59be027a0d8775fd400382c43fd627299e680
Nithyashreeks11 <yasodanithya@gmail.com>
2025-11-15 11:45:05 +0530

    Create areaofcircleopp.py

---
+++ areaofcircleopp.py
@@ -0,0 +1,8 @@
+class areaofcircle:
+  def __init__(self,pi,radius):
+    self.pi=pi
+    self.radius=radius
+  def areaofcircle(self):
+    return self.pi*self.radius*self.radius
+obj=areaofcircle(3.14,10)
+obj.areaofcircle( )
